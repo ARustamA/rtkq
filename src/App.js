cb3164bff27a310b6770a7c70af92b1dabf22143
“ARustamA” <rakhmetyanov@gmail.com>
2022-11-10 19:40:55 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { useGetGoodsQuery } from './redux/goodsApi';
 
 function App() {
+  const { data = [], isLoading } = useGetGoodsQuery();
   return (
-    <>
-    </>
+    <div>
+      {isLoading &&
+        <h2 className=' text-center bg-red-500 font-serif'>
+          Loading...</h2>}
+      <ul className='list-item'>
+        {data.map(item => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
